502357545d6ea6bcbf5a8cdbb904597795a0dce2
Hela001 <neflahela@gmail.com>
2023-05-02 16:04:10 +0100

    first commit

---
+++ CM4/Core/Src/main.c
@@ -0,0 +1,380 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include <stdint.h>
+
+#define HTS221_ADDRESS     0xBE  // I2C address of the HST221 sensor
+
+// HTS221 register addresses
+#define HTS221_WHO_AM_I    0x0F
+#define HTS221_CTRL_REG1   0x20
+#define HTS221_CTRL_REG2   0x21
+#define HTS221_CTRL_REG3   0x22
+#define HTS221_STATUS_REG  0x27
+#define HTS221_HUMIDITY_OUT_L 0x28
+#define HTS221_HUMIDITY_OUT_H 0x29
+#define HTS221_TEMP_OUT_L     0x2A
+#define HTS221_TEMP_OUT_H     0x2B
+
+// HTS221 bit masks
+#define HTS221_PD_BIT      0x80
+#define HTS221_BDU_BIT     0x04
+#define HTS221_ODR_BIT     0x03
+#define HTS221_ODR_ONE_SHOT 0x00
+#define HTS221_ODR_1_HZ    0x01
+#define HTS221_ODR_7_HZ    0x02
+#define HTS221_ODR_12_5_HZ 0x03
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+float temperature,humidity;
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+I2C_HandleTypeDef hi2c1;
+
+UART_HandleTypeDef huart2;
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USART2_UART_Init(void);
+static void MX_I2C1_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+// Function to read a single byte from the HTS221 sensor over I2C
+uint8_t HTS221_ReadReg(uint8_t reg) {
+    uint8_t data;
+    HAL_I2C_Mem_Read(&hi2c1, (0xBF<<1), reg, I2C_MEMADD_SIZE_8BIT, &data, 1, 1000);
+    //HAL_I2C_Mem_Read(&hi2c1, (HTS221_ADDRESS, reg, I2C_MEMADD_SIZE_8BIT, &data, 1, 1000);
+
+    return data;
+}
+
+// Function to write a single byte to the HTS221 sensor over I2C
+void HTS221_WriteReg(uint8_t reg, uint8_t data) {
+    HAL_I2C_Mem_Write(&hi2c1, (0xBE<<1)+0, reg, I2C_MEMADD_SIZE_8BIT, &data, 1, 1000);
+}
+
+// Function to initialize the HTS221 sensor
+void HTS221_Init() {
+    // Read the WHO_AM_I register to verify communication with the sensor
+    uint8_t who_am_i = HTS221_ReadReg(HTS221_WHO_AM_I);
+    if (who_am_i != 0xBC) {
+        // Error: WHO_AM_I register value does not match expected value
+        return;
+    }
+
+    // Configure the control registers for one-shot measurement mode, 12.5 Hz output data rate, and block data update
+    HTS221_WriteReg(HTS221_CTRL_REG1, HTS221_ODR_ONE_SHOT | HTS221_PD_BIT);
+    HTS221_WriteReg(HTS221_CTRL_REG2, 0x00);
+    HTS221_WriteReg(HTS221_CTRL_REG3, 0x00);
+
+    // Wait for the sensor to be ready for measurements
+   // while (!(HTS221_ReadReg(HTS221_STATUS_REG) & 0x01));
+}
+void HTS221_ReadData(float temperature, float humidity) {
+    // Start a one-shot measurement by writing to the control register
+    uint8_t ctrl_reg1 = HTS221_ReadReg( HTS221_CTRL_REG1);
+    ctrl_reg1 |= 0x85; // Set PD bit to 1 and BDU bit to 1
+    HTS221_WriteReg(HTS221_CTRL_REG1, ctrl_reg1);
+
+    // Wait for the measurement to complete
+  //  while (!(HTS221_ReadReg(HTS221_STATUS_REG) & 0x01));
+
+    // Read the humidity data (two bytes)
+    uint16_t humidity_raw = (HTS221_ReadReg(HTS221_HUMIDITY_OUT_H) << 8) | HTS221_ReadReg(HTS221_HUMIDITY_OUT_L);
+
+    // Read the temperature data (two bytes)
+    uint16_t temperature_raw = (HTS221_ReadReg(HTS221_TEMP_OUT_H) << 8) | HTS221_ReadReg(HTS221_TEMP_OUT_L);
+
+    // Convert the raw data to temperature and humidity values
+    humidity = (float)humidity_raw / 65536.0 * 100.0;
+    temperature = (float)temperature_raw / 65536.0 * 165.0 - 40.0;
+}
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART2_UART_Init();
+  MX_I2C1_Init();
+  /* USER CODE BEGIN 2 */
+  HTS221_Init();
+  /* USER CODE END 2 */
+
+  /* Boot CPU2 */
+  HAL_PWREx_ReleaseCore(PWR_CORE_CPU2);
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+	  HTS221_ReadData(temperature, humidity);
+	  HAL_Delay(500);
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure LSE Drive Capability
+  */
+  HAL_PWR_EnableBkUpAccess();
+  __HAL_RCC_LSEDRIVE_CONFIG(RCC_LSEDRIVE_LOW);
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_LSE|RCC_OSCILLATORTYPE_MSI;
+  RCC_OscInitStruct.LSEState = RCC_LSE_ON;
+  RCC_OscInitStruct.MSIState = RCC_MSI_ON;
+  RCC_OscInitStruct.MSICalibrationValue = RCC_MSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_6;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure the SYSCLKSource, HCLK, PCLK1 and PCLK2 clocks dividers
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK3|RCC_CLOCKTYPE_HCLK2
+                              |RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_MSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.AHBCLK2Divider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.AHBCLK3Divider = RCC_SYSCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief I2C1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C1_Init(void)
+{
+
+  /* USER CODE BEGIN I2C1_Init 0 */
+
+  /* USER CODE END I2C1_Init 0 */
+
+  /* USER CODE BEGIN I2C1_Init 1 */
+
+  /* USER CODE END I2C1_Init 1 */
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.Timing = 0x00000E14;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Analogue filter
+  */
+  if (HAL_I2CEx_ConfigAnalogFilter(&hi2c1, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Digital filter
+  */
+  if (HAL_I2CEx_ConfigDigitalFilter(&hi2c1, 0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C1_Init 2 */
+
+  /* USER CODE END I2C1_Init 2 */
+
+}
+
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart2.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart2.Init.ClockPrescaler = UART_PRESCALER_DIV1;
+  huart2.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_UARTEx_SetTxFifoThreshold(&huart2, UART_TXFIFO_THRESHOLD_1_8) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_UARTEx_SetRxFifoThreshold(&huart2, UART_RXFIFO_THRESHOLD_1_8) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_UARTEx_DisableFifoMode(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
---
+++ CM4/Debug/Core/Library_files/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Core/Library_files/HTS221_anklabs.c 
+
+OBJS += \
+./Core/Library_files/HTS221_anklabs.o 
+
+C_DEPS += \
+./Core/Library_files/HTS221_anklabs.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/Library_files/%.o Core/Library_files/%.su: ../Core/Library_files/%.c Core/Library_files/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DCORE_CM4 -DUSE_HAL_DRIVER -DSTM32WL55xx -c -I../Core/Inc -I"C:/Users/Hela/OneDrive/Bureau/stm32WL-bareMetal_workspace/ht221/CM4/Core/Library_files" -I"C:/Users/Hela/OneDrive/Bureau/stm32WL-bareMetal_workspace/ht221/CM4/Core/Library_files/HAL_API_for_HTS221" -I../../Drivers/STM32WLxx_HAL_Driver/Inc -I../../Drivers/STM32WLxx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32WLxx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
+clean: clean-Core-2f-Library_files
+
+clean-Core-2f-Library_files:
+	-$(RM) ./Core/Library_files/HTS221_anklabs.d ./Core/Library_files/HTS221_anklabs.o ./Core/Library_files/HTS221_anklabs.su
+
+.PHONY: clean-Core-2f-Library_files
+
